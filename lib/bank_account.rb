8bae3b7f2bbd435561b22b22e26e60e7dd392f65
Tony Distinti <adistinti@gmail.com>
2017-09-30 11:08:34 +0000

    automatically backed up by learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -10,5 +10,20 @@ class BankAccount
     @status = "open"
   end
 
+  def desposit(amount)
+    self.balance += amount
+  end
+
+  def display_balance
+    "Your balance is $#{self.balance}."
+  end
+
+  def valid?
+    self.status == "open" && self.balance > 0
+  end
+
+  def close_account
+    self.status = "closed"
+  end
   
 end
